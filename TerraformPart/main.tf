012f12060aea096a4c82db5a1b3dd3c5d2d11707
AlecsandraMuha <alecsandramuha@yahoo.com>
2023-08-09 10:57:06 +0300

    Terraform infrastructure working

--- TerraformPart/main.tf
+++ TerraformPart/main.tf
@@ -9,25 +9,6 @@ resource "google_container_cluster" "gke_cluster" {
   initial_node_count = 2
 }
 
-resource "google_container_node_pool" "primary_pool" {
-  name       = "default-pool-gke"
-  cluster    = google_container_cluster.gke_cluster.name
-  location   = google_container_cluster.gke_cluster.location
-  node_count = 3
-
-  autoscaling {
-    min_node_count = 2
-    max_node_count = 5
-  }
-
-
-  node_config {
-    machine_type = "n1-standard-1"
-    disk_size_gb = 100
-    #location     = "europe-west1-b"
-  }
-}
-
 resource "google_sql_database_instance" "my_database" {
   name             = "my-database"
   database_version = "MYSQL_8_0"
